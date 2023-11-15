230a0502a864467caaea77146d6cf5668e83e796
erickH1999 <92340704+erickH1999@users.noreply.github.com>
2023-11-15 12:39:04 -0500

    Add files via upload
    This project is an example of my work and experience in handling data structures.

---
+++ LinkedStack.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+
+    public class LinkedStack
+    {
+
+        private Node top;
+        private int size;
+
+        public LinkedStack()
+        {
+            top = null;
+            size = 0;
+        }
+
+        public bool isEmpty()
+        {
+            return top == null;
+        }
+
+        public void Push(int data)
+        {
+                       
+            Node newNode = new Node(data);
+
+            if (top == null) 
+            {
+                top = newNode;
+            }
+            else
+            {
+                newNode.next = top;
+                top = newNode;
+            }
+
+
+        }
+
+        public void Pop()
+        {
+
+        }
+
+        public int Peek()
+        {
+            if (isEmpty())
+            {
+                throw new Exception("Stack is Empty!!");
+            }
+
+            return top.data;
+        }
+
+        public class Node 
+        {
+            public int data;
+            public Node? next;
+
+            public Node(int data)
+            {
+                this.data = data;
+                this.next = null;
+            }
+        }
+
+        
+    }
+
+    
+}
